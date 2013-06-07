ab12a67c2f001934733c4a7cc8c152387480a7ef
nowrep <nowrep@gmail.com>
2013-06-07 17:07:33 +0200

    Added Event class.

---
+++ src/event.cpp
@@ -0,0 +1,235 @@
+/*
+ * This file is part of QZeitgeist.
+ *
+ * Copyright (C) 2013 David Rosca <nowrep@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+extern "C" {
+#include <zeitgeist.h>
+}
+
+#include "event.h"
+#include <QtCore/QUrl>
+
+namespace QZeitgeist
+{
+
+static GPtrArray *convertToPtrArray(const QList<Subject> &subjects)
+{
+    GPtrArray *array = g_ptr_array_sized_new(subjects.size());
+
+    foreach (const Subject &subject, subjects) {
+        g_ptr_array_add(array, (gpointer)subject.createHandle());
+    }
+
+    return array;
+}
+
+
+class EventPrivate
+{
+public:
+    explicit EventPrivate();
+    bool compare(const EventPrivate &other) const;
+    void copy(const EventPrivate &other);
+
+    quint32 id;
+    qint64 timestamp;
+    QUrl origin;
+    QUrl actor;
+    QUrl interpretation;
+    QUrl manifestation;
+    QByteArray payload;
+    QList<Subject> subjects;
+};
+
+EventPrivate::EventPrivate()
+    : id(0)
+    , timestamp(0)
+{
+}
+
+bool EventPrivate::compare(const EventPrivate &other) const
+{
+    return id == other.id
+           && timestamp == other.timestamp
+           && origin == other.origin
+           && actor == other.actor
+           && interpretation == other.interpretation
+           && manifestation == other.manifestation
+           && payload == other.payload
+           && subjects == other.subjects;
+}
+
+void EventPrivate::copy(const EventPrivate &other)
+{
+    if (this != &other) {
+        id = other.id;
+        timestamp = other.timestamp;
+        origin = other.origin;
+        actor = other.actor;
+        interpretation = other.interpretation;
+        manifestation = other.manifestation;
+        payload = other.payload;
+        subjects = other.subjects;
+    }
+}
+
+// class Event
+Event::Event()
+    : d(new EventPrivate)
+{
+}
+
+Event::Event(const Event &other)
+    : d(new EventPrivate)
+{
+    d->copy(*other.d);
+}
+
+Event::~Event()
+{
+}
+
+Event &Event::operator=(const Event &other)
+{
+    d->copy(*other.d);
+    return *this;
+}
+
+bool Event::operator==(const Event &other) const
+{
+    return d->compare(*other.d);
+}
+
+bool Event::isValid() const
+{
+    return d->id > 0 && d->timestamp > 0;
+}
+
+quint32 Event::id() const
+{
+    return d->id;
+}
+
+void Event::setId(quint32 id)
+{
+    d->id = id;
+}
+
+qint64 Event::timestamp() const
+{
+    return d->timestamp;
+}
+
+void Event::setTimestamp(qint64 timestamp)
+{
+    d->timestamp = timestamp;
+}
+
+QUrl Event::origin() const
+{
+    return d->origin;
+}
+
+void Event::setOrigin(const QUrl &origin)
+{
+    d->origin = origin;
+}
+
+QUrl Event::actor() const
+{
+    return d->actor;
+}
+
+void Event::setActor(const QUrl &actor)
+{
+    d->actor = actor;
+}
+
+QUrl Event::interpretation() const
+{
+    return d->interpretation;
+}
+
+void Event::setInterpretation(const QUrl &interpretation)
+{
+    d->interpretation = interpretation;
+}
+
+QUrl Event::manifestation() const
+{
+    return d->manifestation;
+}
+
+void Event::setManifestation(const QUrl &manifestation)
+{
+    d->manifestation = manifestation;
+}
+
+QByteArray Event::payload() const
+{
+    return d->payload;
+}
+
+void Event::setPayload(const QByteArray &payload)
+{
+    d->payload = payload;
+}
+
+QList<Subject> Event::subjects() const
+{
+    return d->subjects;
+}
+
+void Event::setSubjects(const QList<Subject> &subjects)
+{
+    d->subjects = subjects;
+}
+
+void Event::removeSubject(const Subject &subject)
+{
+    d->subjects.removeOne(subject);
+}
+
+void Event::addSubject(const Subject &subject)
+{
+    d->subjects.append(subject);
+}
+
+HANDLE Event::createHandle() const
+{
+    QByteArray originData = d->origin.toString().toUtf8();
+    QByteArray actorData = d->actor.toString().toUtf8();
+    QByteArray interpretationData = d->interpretation.toString().toUtf8();
+    QByteArray manifestationData = d->manifestation.toString().toUtf8();
+    GPtrArray *subjects = convertToPtrArray(d->subjects);
+
+    ::ZeitgeistEvent *event = zeitgeist_event_new();
+    zeitgeist_event_set_id(event, d->id);
+    zeitgeist_event_set_timestamp(event, d->timestamp);
+    zeitgeist_event_set_origin(event, originData.constData());
+    zeitgeist_event_set_actor(event, actorData.constData());
+    zeitgeist_event_set_interpretation(event, interpretationData.constData());
+    zeitgeist_event_set_manifestation(event, manifestationData.constData());
+    zeitgeist_event_set_subjects(event, subjects);
+
+    // Is it necessary here?
+    g_ptr_array_unref(subjects);
+}
+
+}; // namespace QZeitgeist
+
---
+++ src/event.h
@@ -0,0 +1,81 @@
+/*
+ * This file is part of QZeitgeist.
+ *
+ * Copyright (C) 2013 David Rosca <nowrep@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef QZEITGEIST_EVENT_H_
+#define QZEITGEIST_EVENT_H_
+
+#include "qzeitgeist.h"
+#include "subject.h"
+#include <QtCore/QScopedPointer>
+#include <QtCore/QList>
+
+class QUrl;
+
+namespace QZeitgeist
+{
+
+class EventPrivate;
+
+class QZEITGEIST_EXPORT Event
+{
+public:
+    Event();
+    Event(const Event &other);
+    ~Event();
+
+    Event &operator=(const Event &other);
+    bool operator==(const Event &other) const;
+
+    bool isValid() const;
+
+    quint32 id() const;
+    void setId(quint32 id);
+
+    qint64 timestamp() const;
+    void setTimestamp(qint64 timestamp);
+
+    QUrl origin() const;
+    void setOrigin(const QUrl &origin);
+
+    QUrl actor() const;
+    void setActor(const QUrl &actor);
+
+    QUrl interpretation() const;
+    void setInterpretation(const QUrl &interpretation);
+
+    QUrl manifestation() const;
+    void setManifestation(const QUrl &manifestation);
+
+    QByteArray payload() const;
+    void setPayload(const QByteArray &payload);
+
+    QList<Subject> subjects() const;
+    void setSubjects(const QList<Subject> &subjects);
+    void removeSubject(const Subject &subject);
+    void addSubject(const Subject &subject);
+
+    HANDLE createHandle() const;
+
+private:
+    QScopedPointer<EventPrivate> d;
+};
+
+}; // namespace QZeitgeist
+
+#endif // QZEITGEIST_EVENT_H_
