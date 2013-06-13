5fd1f196fdcba20d53e16a1289bc9967e1a6c479
nowrep <nowrep@gmail.com>
2013-06-13 14:32:14 +0200

    Added ResultSet class

---
+++ src/resultset.cpp
@@ -0,0 +1,76 @@
+/*
+ * This file is part of QZeitgeist.
+ *
+ * Copyright (C) 2013 David Rosca <nowrep@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+extern "C" {
+#include <zeitgeist.h>
+}
+
+#include "resultset.h"
+#include "event.h"
+#include <QtCore/QDebug>
+
+namespace QZeitgeist
+{
+
+ResultSet::ResultSet(HANDLE handle)
+    : m_handle(handle)
+{
+    Q_ASSERT(m_handle);
+
+    g_object_ref((ZeitgeistResultSet *)m_handle);
+}
+
+ResultSet::~ResultSet()
+{
+    g_object_unref((ZeitgeistResultSet *)m_handle);
+}
+
+int ResultSet::size() const
+{
+    return (int) zeitgeist_result_set_size((ZeitgeistResultSet *)m_handle);
+}
+
+int ResultSet::estimatedMatches() const
+{
+    return (int) zeitgeist_result_set_estimated_matches((ZeitgeistResultSet *)m_handle);
+}
+
+Event ResultSet::nextValue()
+{
+    ZeitgeistEvent *ev = zeitgeist_result_set_next_value((ZeitgeistResultSet *)m_handle);
+    return Event::fromHandle(ev);
+}
+
+bool ResultSet::hasNext() const
+{
+    return zeitgeist_result_set_has_next((ZeitgeistResultSet *)m_handle);
+}
+
+int ResultSet::currentPosition() const
+{
+    return (int) zeitgeist_result_set_tell((ZeitgeistResultSet *)m_handle);
+}
+
+void ResultSet::reset()
+{
+    zeitgeist_result_set_reset((ZeitgeistResultSet *)m_handle);
+}
+
+}; // namespace QZeitgeist
+
---
+++ src/resultset.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of QZeitgeist.
+ *
+ * Copyright (C) 2013 David Rosca <nowrep@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef QZEITGEIST_RESULTEST_H_
+#define QZEITGEIST_RESULTEST_H_
+
+#include "qzeitgeist.h"
+
+namespace QZeitgeist
+{
+
+class Event;
+
+class QZEITGEIST_EXPORT ResultSet
+{
+public:
+    ResultSet(HANDLE handle);
+    ~ResultSet();
+
+    int size() const;
+    int estimatedMatches() const;
+
+    Event nextValue();
+    bool hasNext() const;
+
+    int currentPosition() const;
+    void reset();
+
+private:
+    // Disable copying
+    ResultSet(const ResultSet &other);
+    ResultSet &operator=(const ResultSet &other);
+
+    HANDLE m_handle;
+};
+
+}; // namespace QZeitgeist
+
+#endif // QZEITGEIST_RESULTEST_H_
